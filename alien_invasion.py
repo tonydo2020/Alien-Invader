c7ea10f283a2261767edf4011add3ae20d3bd064
tonydo2020 <55472788+tonydo2020@users.noreply.github.com>
2019-09-23 16:18:56 -0700

    Updated Files
    Stopped at Shooing Bullets, page 257

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
+
 
 def run_game():
     # Initialize game and create a screen object.
@@ -12,11 +14,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Start the main loop for the game.
     while True:
-
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
         # Redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
         ship.blitme()
