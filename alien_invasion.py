eb093cef09331e79adeffb3a1cd619ddf8418136
tonydo2020 <55472788+tonydo2020@users.noreply.github.com>
2019-09-26 02:08:01 -0700

    Score board and play options. Project Finished

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,48 +1,54 @@
-import sys
-
 import pygame
 from pygame.sprite import Group
+
 from settings import Settings
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 
 def run_game():
-    # Initialize game and create a screen object.
+    # Initialize pygame, settings, and screen object.
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(ai_settings, screen)
-    # Make a group to store bullets in
-    bullets = Group()
+    # Make the Play button.
+    play_button = Button(ai_settings, screen, "Play")
 
-    # Make an alien
+    # Create an instance to store game statistics, and a scoreboard.
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
+    # Set the background color.
+    bg_color = (230, 230, 230)
+
+    # Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
     aliens = Group()
 
-    #Creates the fleet of aliens
+    # Create the fleet of aliens.
     gf.create_fleet(ai_settings, screen, ship, aliens)
+
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        #gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship,
+                        aliens, bullets)
 
-            # Get rid of te bullets that have disappeared.
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-        print(len(bullets))
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens,
+                              bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens,
+                             bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-        # Redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens,
+                         bullets, play_button)
 
 
 run_game()
