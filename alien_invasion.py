5d566bae319e65e1a9db45a73dd3383f0b139ddc
tonydo2020 <55472788+tonydo2020@users.noreply.github.com>
2019-09-25 22:48:18 -0700

    Got Alien to appear

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,10 @@
 import sys
 
 import pygame
-
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 
@@ -15,12 +16,26 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in
+    bullets = Group()
+
+    # Make an alien
+    alien = Alien(ai_settings, screen)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+
+            # Get rid of te bullets that have disappeared.
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
+
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
         # Redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
         ship.blitme()
